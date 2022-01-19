a794988be14b7319dc53af136866bf071c411084
xelecha <xelecha@gmail.com>
2022-01-19 18:26:38 +0100

    Visor Catastro

--- js/edificios.js
+++ js/edificios.js
@@ -14,7 +14,7 @@ function addEdificiosCapa() {
     "source-layer": "construcciones-c14i2x", // Nuestro nombre Tileset
     "maxzoom": 21,
     "minzoom": 15,
-   // "filter": [">", "numberOfFl", 0],
+    "filter": [">", "numberOfFl", 0],
     "paint": {
         "fill-extrusion-color": [
             "interpolate", ["linear"], ["number", ["get", "numberOfFl"]],
@@ -48,3 +48,65 @@ function addEdificiosCapa() {
 );  
 
 } //fin funcion
+
+function filtrarEdificios(valor) {
+    map.setFilter("edificios", [">", "numberOfFl", parseInt(valor)]);
+
+    document.getElementById("altura").innerHTML = "Más de  <b>" + valor + "</b> pisos";
+
+}
+
+function verEdificios (visible){
+
+    if(visible==true){
+    map.setLayoutProperty("edificios", "visibility", "visible");}
+    else{
+    map.setLayoutProperty("edificios", "visibility", "none");
+    }
+
+}
+
+function addPopupToMapEdificios(nombreCapa) {
+
+    map.on('click', nombreCapa, function (e) {
+
+        var text = "";
+        //console.info(e);
+        for (key in e.features[0].properties) {
+
+            if (key == "numberOfFl") {
+                text += "<b>Numero de plantas</b>:" + e.features[0].properties[key] + "<br>";
+            }
+            if (key == "localId") {
+                //localId 0004702DF3800C_part1
+                //http://ovc.catastro.meh.es/OVCServWeb/OVCWcfLibres/OVCFotoFachada.svc/RecuperarFotoFachadaGet?ReferenciaCatastral=0004701DF3800C
+                //https://www1.sedecatastro.gob.es/CYCBienInmueble/OVCListaBienes.aspx?rc1=0004701&rc2=DF3800C
+
+                var localId = e.features[0].properties[key];
+
+                var localIdSplit = localId.split("_"); // 0004702DF3800C  part1
+                var parte1 = localIdSplit[0].substring(0, 7);
+                var parte2 = localIdSplit[0].substring(7, localIdSplit[0].length);
+                text += "<img width=200 src=http://ovc.catastro.meh.es/OVCServWeb/OVCWcfLibres/OVCFotoFachada.svc/RecuperarFotoFachadaGet?ReferenciaCatastral=" + localId + "><br>";
+                text += "<a target=blank href=https://www1.sedecatastro.gob.es/CYCBienInmueble/OVCListaBienes.aspx?rc1=" + parte1 + "&rc2=" + parte2 + ">Ficha</a><br>";
+
+            }
+
+
+        }
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML(text)
+            .addTo(map);
+
+    });
+
+    map.on('mouseenter', nombreCapa, function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    map.on('mouseleave', nombreCapa, function () {
+        map.getCanvas().style.cursor = '';
+    });
+
+}
