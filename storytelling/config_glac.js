d1c6c0923c32d4c4b7b46e499cb5814777733725
xelecha <xelecha@gmail.com>
2022-02-22 10:25:37 +0100

    Glaciares

---
+++ storytelling/config_glac.js
@@ -0,0 +1,141 @@
+var config = {
+    style: 'mapbox://styles/xelecha/ckzv8nzca000x14p81xvrnoqq',
+    accessToken: 'pk.eyJ1IjoieGVsZWNoYSIsImEiOiJja3lpeDl2YnIyZ3Z3Mm5uMHFqZHZzd3JoIn0.rijVtBx79_xWHiJgDc41iA',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    theme: 'dark',
+    use3dTerrain: true,
+    title: 'Glaciares',
+    subtitle: 'Breve recorrido por algunos de los glaciares más espectaculares de la Tierra',
+    byline: 'Xenia Lecha Mora - Master SIG UPC',
+    footer: 'Fuentes: Wikipedia - ARCA - GLIMS - NASA',
+    chapters: [
+        {
+            id: 'glaciar1',
+            alignment: 'left',
+            hidden: false,
+            title: 'Vatnajökull, Islandia',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/8/8f/Vatnaj%C3%B6kull.jpeg',
+            description: '<p><b>Vatnajökull</b> es el mayor glaciar de Islandia y el segundo de Europa. Cubre un area de 8.100 km² al sudeste del país y corresponde a más del 8 por ciento de la superficie total de la isla. Es el mayor glaciar de Europa en volumen (unos 3.000 km³) y el segundo en área, sólo superado por <a href="https://es.wikipedia.org/wiki/Austfonna">Austfonna</a> en <a href="https://es.wikipedia.org/wiki/Nordaustlandet">Nordaustlandet</a>, en el archipiélago de las <a href="https://es.wikipedia.org/wiki/Svalbard">Svalbard</a> (Noruega).</p><p>El <b>Vatnajökull</b> ha experimentado un continuo retroceso, posiblemente debido al calentamiento global y a la reciente actividad volcánica en el área. En 1980 tenía 8.300 km² mientras que en 1958, 8.538 km².</p>',
+            location: {
+                center: [-17.294702860760264, 64.25913036599823],
+                zoom: 9,
+                pitch: 60,
+                bearing: -50
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },{
+            id: 'glaciar2',
+            alignment: 'right',
+            hidden: false,
+            title: 'Aletsch, Suiza',
+            image: 'https://cdn.getyourguide.com/img/tour/5cd516b88e7ea.jpeg/98.jpg',
+            description: '<p>El glaciar <b>Aletsch</b> o Gran Glaciar Aletsch (en alemán: Grosser Aletschgletscher) es el glaciar más grande de los Alpes, con 23 km de longitud y más de 120 km². Localizado en el suroeste de Suiza, en el cantón de <a href="https://es.wikipedia.org/wiki/Cant%C3%B3n_del_Valais">Valais</a>, dentro del espacio <a href="https://es.wikipedia.org/wiki/Jungfrau-Aletsch-Bietschhorn">Jungfrau-Aletsch-Bietschhorn</a> fue declarado Patrimonio de la Humanidad por la Unesco en 2001.</p><p>Los orígenes del glaciar <b>Aletsch</b> se sitúan en los 4.000 m de altura en la cara sur del macizo del Jungfrau, donde se generan separadas tres grandes lenguas, que confluyen algo más abajo en la denominada Plaza de la Concordia (Konkordiaplatz), una extensión de unos 6 km² que se considera propiamente el inicio del glaciar.</p><p>Debido a su gran tamaño, el glaciar <b>Aletsch</b> es relativamente inmune a las fluctuaciones climáticas a corto plazo. Mientras muchos otros glaciares avanzaron de finales de los 70 a principios de los 80, el glaciar <b>Aletsch</b> apenas reaccionó, como tampoco ha reaccionado al calentamiento posterior. Aún así, en los últimos años sí está sufriendo las consecuencias de las elevadas temperaturas y está en claro retroceso, igual que el resto de los glaciares alpinos.</p>',
+            location: {
+                center: [8.132318767445213, 46.52829513810475],
+                zoom: 11,
+                pitch: 70,
+                bearing: -100
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },{
+            id: 'glaciar3',
+            alignment: 'left',
+            hidden: false,
+            title: 'Lambert, Antartica',
+            image: 'https://media.nationalgeographic.org/assets/photos/000/313/31385.jpg',
+            description: '<p>El glaciar Lambert es un glaciar de la Antártida Oriental. Con 100 km de ancho, más de 400 km de largo y cerca de 2.500 m de profundidad figura en el Libro Guinness de los récords como el glaciar más grande del mundo. Drena el 8% de la capa de hielo de la Antártida al este y al sur de las montañas Prince Charles y fluye hacia el norte a la Barrera de hielo Amery. Fluye en parte de la fosa tectónica Graben y sale del continente en la bahía Prydz.</p><p>Este glaciar es importante en el estudio del cambio climático porque muy pequeños cambios en el clima pueden tener consecuencias significativas para el flujo del hielo glaciar abajo.2​ La mayor parte de los estudios en el glaciar son hechos por medio de teledetección debido a las duras condiciones climatológicas del área.</p>',
+            location: {
+                center: [74.11145133565712, -72.01072409134025],
+                zoom: 5,
+                pitch: 60,
+                bearing: -210
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },{
+            id: 'glaciar4',
+            alignment: 'right',
+            hidden: false,
+            title: 'Hubbard, Alaska',
+            image: 'https://travelingcanucks.com/wp-content/uploads/2019/09/alaska-cruise-0006.jpg',
+            description: '<p>El Glaciar Hubbard es un glaciar situado entre las Montañas San Elías en Alaska (Estados Unidos) y el Territorio de Yukón (Canadá).</p><p>Desde su origen en el Territorio de Yukon el glaciar se extiende 122 km hasta la bahía de Disenchantment y la bahía de Yakutat. Eso le hace el glaciar más largo en Alaska con terminación en el agua, mientras que su fachada mide más de diez km.</p><p><video autoplay=true loop=true width="370"><source src="https://svs.gsfc.nasa.gov/vis/a010000/a013400/a013492/Hubbard_1972_2019_8fps_1080p.webm" type="video/webm"></video></p><p>En este timelapse (parte de un <a href="https://svs.gsfc.nasa.gov/13492">estudio de glaciares de Alaska</a> realizado a lo largo de 48 años por NASA) se pueden observar los movimientos del Hubbard de 1972 a 2019.</p>',
+            location: {
+                center: [-139.12069459382542, 60.073361723687036],
+                zoom: 9,
+                pitch: 60,
+                bearing: -20
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },{
+            id: 'glaciar5',
+            alignment: 'left',
+            hidden: false,
+            title: 'Fedchenko, Tajikistan',
+            image: 'https://previews.123rf.com/images/michalknitl/michalknitl1501/michalknitl150100541/35843409-magn%C3%ADfico-glaciar-fedchenko-en-las-monta%C3%B1as-pamir-en-tayikist%C3%A1n.jpg',
+            description: '<p>El glaciar Fedchenko (en ruso: Федченко ледник) es un gran glaciar en las montañas de Pamir, del centro-norte de Gorno-Badakhshan, en el país asiático de Tajikistán. El glaciar es largo y estrecho, en la actualidad se extiende a lo largo de 77 kilómetros (48 millas) y abarca más de 700 kilómetros cuadrados (270 millas cuadradas). Es el glaciar más largo del mundo fuera de las regiones polares.</p>',
+            location: {
+                center: [72.44752631703845, 39.00617061686797],
+                zoom: 10,
+                pitch: 60,
+                bearing: 100
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },{
+            id: 'glaciar6',
+            alignment: 'right',
+            hidden: false,
+            title: 'Brüggen, Chile',
+            image: 'https://lh5.googleusercontent.com/p/AF1QipM2tkjBvEbfnSRttOWGcoktTYKLH_pWsi2cJtzk=w408-h306-k-no',
+            description: '<p>El glaciar Pío XI, también conocido como glaciar Brüggen o Ana María se ubica en la Región de Magallanes, en la zona austral de Chile. Forma parte del conjunto de glaciares que se desprenden del Campo de Hielo Sur, siendo el mayor de todos ellos y de Sudamérica con sus 1265 km² de superficie.</p>',
+            location: {
+                center: [-73.79144754657536, -49.45489837578567],
+                zoom: 9,
+                pitch: 60,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },{
+            id: 'glaciar7',
+            alignment: 'left',
+            hidden: false,
+            title: 'Franz Josef, New Zealand',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Franz_Josef_glacier.JPG/528px-Franz_Josef_glacier.JPG',
+            description: '<p>El Franz Josef (Ka Roimata o Hinehukatere en maorí) es un glaciar de 12 km de largo ubicado en Parque nacional Westland en la costa oeste de la Isla Sur de Nueva Zelanda. Junto con el Glaciar Fox de 20 km al sur, que es único en bajar de los Alpes del Sur a menos de 300 metros sobre el nivel del mar, en medio del verdor y frondosidad de un bosque templado lluvioso.</p><p>El área que rodea a los dos glaciares es parte de Te Wahipounamu, un parque catalogado como Patrimonio de la Humanidad. El río que emerge de la terminal del glaciar Franz Josef se conoce como el río Waiho.</p><p>Después de haber retrocedido varios kilómetros entre los años 1940 y 1980, el glaciar entró en una fase de avance en 1984, y en ocasiones ha avanzado a un ritmo espectacular (para los estándares glaciales) de 70 cm al día.</p><p>El glaciar seguía avanzando hasta 2008, pero desde entonces ha entrado en una fase de rápido retroceso. Como es el caso para la mayoría de los glaciares de Nueva Zelanda que se encuentran principalmente en la parte oriental de los Alpes del Sur, el proceso de contracción se atribuye al calentamiento global.</p>',
+            location: {
+                center: [170.2166315622821, -43.4220848140082],
+                zoom: 12,
+                pitch: 70,
+                bearing: 50
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
